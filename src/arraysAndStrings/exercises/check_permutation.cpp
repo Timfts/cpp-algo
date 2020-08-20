2972cad6898576fa8c1d6f5d996e2c10996c539e
Italo Fontes <italo5123@hotmail.com>
2020-08-20 01:39:39 -0300

    :construction: learning pointers inside algorithms

--- src/arraysAndStrings/exercises/check_permutation.cpp
+++ src/arraysAndStrings/exercises/check_permutation.cpp
@@ -3,16 +3,39 @@
 using namespace std;
 
 /* Given two strings, write a method to decide if one is a permutation of the other */
+// try to do using basic types
 
+bool checkPermutation(const string firstString, const string secondString, const bool useExtendedASCII)
+{
+  if (firstString.length() != secondString.length())
+    return false;
 
+  const int charsetSize = useExtendedASCII ? 256 : 128;
 
-bool checkPermutation(const string firstString, const string secondString) {
-  if(firstString.length() != secondString.length()) return false;
+  auto fillCharMap = [](int *charMap, string wordToMap, int charsetSize = 128) {
+    int *charMapCopy = charMap;
+    for (int c = 0; c < wordToMap.length(); c++)
+    {
+      int currentCharNumber = wordToMap[c];
+      charMapCopy[currentCharNumber] += 1;
+    }
+    return charMapCopy;
+  };
 
+  auto compareChars = []() {
+    cout << "compare second word with map" << endl;
+  };
+
+  int *charMap = new int[charsetSize];
+  int *filledCharMap = fillCharMap(charMap, firstString, charsetSize);
+  delete[] charMap;
+
+  // match filledCharMap with second word
+  delete[] filledCharMap;
   return true;
 }
 
-int main(){
-  bool values = checkPermutation("cenoura", "batatax");
-  cout << values << endl;
+int main()
+{
+  checkPermutation("cenoaaa", "batatax", true);
 }
